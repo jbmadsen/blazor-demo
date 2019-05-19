8591d595a5e4914dbad5e1224b8082487a7a5b3b
jbmadsen <bitbucket@jbmadsen.com>
2019-05-19 16:14:34 +0200

    dotnet new blazor -o src

---
+++ src/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace blazor_demo.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
