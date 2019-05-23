65a016cc4a0d0f042c317ad355deacaf662a71ed
jbmadsen <bitbucket@jbmadsen.com>
2019-05-23 18:48:44 +0200

    I can apparently not spell Sudoku

---
+++ Blazor.Client/Services/ISudokuSolver.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Blazor.Client.Services
+{
+    public interface ISudokuSolver
+    {
+
+    }
+
+
+    public class SudokuSolver : ISudokuSolver
+    {
+        
+    }
+}
