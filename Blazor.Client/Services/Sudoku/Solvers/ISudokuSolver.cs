a4401c418993f1eb7ffdc5eaccc5b3b11329cde8
jbmadsen <bitbucket@jbmadsen.com>
2019-05-29 22:08:18 +0200

    Comments added for a future solver I might want to implement, if I ever get to it

--- Blazor.Client/Services/Sudoku/Solvers/ISudokuSolver.cs
+++ Blazor.Client/Services/Sudoku/Solvers/ISudokuSolver.cs
@@ -2,6 +2,9 @@ using Blazor.Client.Models;
 
 namespace Blazor.Client.Services.Sudoku.Solvers
 {
+    // I might want to look at this solver, and port it to C# :
+    // https://github.com/attractivechaos/plb/blob/master/sudoku/incoming/sudoku_solver.c
+
     public interface ISudokuSolver
     {
         (bool success, SudokuGrid grid) Solve(SudokuGrid grid);
